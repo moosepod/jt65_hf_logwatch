d2c753c6254cbec91cf05748496feb8c92dd0f41
Matthew Christensen <mchristensen@Hamputer.(none)>
2012-03-31 02:44:37 +0000

    Added new importer

--- log/__init__.py
+++ log/__init__.py
@@ -35,18 +35,3 @@ def import_log(filename):
 	r = csv.reader(f)
 	for l in r:
 		print l
-		if l[0] == 'Date': continue
-		if len(l) < 10: continue
-		date, time, qrg, sync, db, dt, df, decoder, exchange = l[0:9]
-		when = datetime.datetime.strptime('%s %s' % (date,time),'%Y-%m-%d %H:%M')
-		try:
-			Entry.objects.get(when=when,exchange=exchange)	
-		except Entry.DoesNotExist:
-			Entry.objects.create(when=when,
-					frequency=qrg,
-					sync=sync,
-					db=db,
-					dt=dt,
-					df=df,
-					decoder=decoder,
-					exchange=exchange)
