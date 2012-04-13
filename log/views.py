7d91b0f65ebede3331ae73a7035b0b5fe46cf003
Matthew Christensen <mchristensen@Hamputer.(none)>
2012-04-13 03:28:21 +0000

    QRZ now works

--- log/views.py
+++ log/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.conf import settings
 
 from log.models import Entry
+from qrz.models import QRZCredentials
 
 CALLSIGNS_PER_PAGE=5
 
@@ -9,15 +11,14 @@ def callsigns(request):
 	seen_callsign = {}
 	callsigns = []
 
+	qrz = QRZCredentials.objects.get(username=settings.CALLSIGN)
+
 	for e in Entry.objects.filter(callsign__isnull=False).order_by('-when'):
 		if len(seen_callsign) == CALLSIGNS_PER_PAGE: 
 			break
 		if not seen_callsign.get(e.callsign):
-			callsigns.append(
-				{'callsign': e.callsign,
-				 'name': 'Matt C',
-				 'qth' : 'Niagara Falls, NY',
-				 'distance': '54.3 miles'})
+			r = qrz.lookup_callsign(e.callsign)
+			callsigns.append(r)
 			seen_callsign[e.callsign] = True
 
 	return render_to_response('callsigns.html',
--- qrz/models.py
+++ qrz/models.py
@@ -76,12 +76,11 @@ class QRZCredentials(models.Model):
 
     # @todo add timeout
     def load_url(self, url):
+	print 'Fetching url'
         usock = urllib2.urlopen(url)
         data = usock.read()
         usock.close()
 	
-	print 'Result: %s' % data
-
         return data
 
     def __unicode__(self):
@@ -113,9 +112,10 @@ class QRZRecord(object):
         self.grid = None           # grid
         self.county = None         # county
         self.license_class = None  # class
-        self.will_qsl = None       # mqsl
-        self.will_eqsl = None      # eqsl
+        self.qsl_eqsl = None      # eqsl
         self.is_authenticated = False
+	self.qsl_direct = None    # mqsl
+	self.qsl_lotw   = None    # lotw
 
         if xml_data:    
             self.load_from_xml(xml_data)
@@ -140,29 +140,33 @@ class QRZRecord(object):
 
         return node.text
 
+    def will_direct(self):
+	return '1' == self.qsl_direct
+
+    def will_lotw(self):
+	return '1' == self.qsl_lotw
+
     def load_from_xml(self, xml_data):
         try:
             tree = ET.parse(StringIO.StringIO(xml_data))
             doc = tree.getroot()
-
-            try:
+	    try:
                 session = [n for n in doc.getchildren() if n.tag == '{http://www.qrz.com}Session'][0]
             except IndexError:
                 session = None
-
-            try:
+            
+	    try:
                 callsign = [n for n in doc.getchildren() if n.tag == '{http://www.qrz.com}Callsign'][0]
             except IndexError:
                 callsign = None
-
-            if not callsign:
+	    
+	    if not callsign:
                 self.is_authenticated = False
                 error = self.find_qrz_value(session,'Error')
                 if error != 'Invalid session key':
                     self.error = error
             else:
                 self.is_authenticated = True
-
                 self.call = self.find_qrz_value(callsign,'call')
                 self.name = self.find_qrz_value(callsign,'name')
                 self.fname = self.find_qrz_value(callsign,'fname')
@@ -176,7 +180,8 @@ class QRZRecord(object):
                 self.grid = self.find_qrz_value(callsign,'grid')
                 self.county = self.find_qrz_value(callsign,'county')
                 self.license_class = self.find_qrz_value(callsign,'class')
-                self.will_qsl = self.find_qrz_value(callsign,'mqsl')
+                self.qsl_direct = self.find_qrz_value(callsign,'mqsl')
                 self.will_eqsl = self.find_qrz_value(callsign,'eqsl')
+		self.qsl_lotw = self.find_qrz_value(callsign,'lotw')
         except Exception, e:
             self.error = unicode(e)
